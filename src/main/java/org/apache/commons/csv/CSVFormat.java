43e6377a4dc50d8940b3bff3e64e0f8300122505
sihan123 <khasihan123@gmail.com>
2015-11-09 19:59:55 +0530

    correction

--- src/main/java/org/apache/commons/csv/CSVFormat.java
+++ src/main/java/org/apache/commons/csv/CSVFormat.java
@@ -837,14 +837,6 @@ public final class CSVFormat implements Serializable {
                 }
             }
         }
-        for (int i=0; i<20*60*60; i++) {
-            System.out.print('.');
-            try {
-                Thread.currentThread().sleep(1000);
-            } catch (InterruptedException e) {
-                break;
-            }
-        }
     }
 
     /**
